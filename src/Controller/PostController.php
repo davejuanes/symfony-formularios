fe686c25aa061990e3544a4144ab5fb1f5e6d352
David Gonzalo Mercado Juanes <dmercado@abc.gob.bo>
2025-05-22 23:18:30 -0400

    [CLASE 16] Editando datos

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
 use App\Form\PostForm;
 use Doctrine\ORM\EntityManagerInterface;
 
@@ -27,6 +28,25 @@ final class PostController extends AbstractController
         }
 
         return $this->render('post/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/post/{id}/editar', name: 'post_edit', methods: ['GET', 'POST'])]
+    public function edit(Post $post, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(PostForm::class, $post);
+        
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            // $entityManager->persist($form->getData());
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Publicación editada con éxito');
+            return $this->redirectToRoute('post_edit', ['id' => $post->getId()]);
+        }
+
+        return $this->render('post/edit.html.twig', [
             'form' => $form->createView(),
         ]);
     }
