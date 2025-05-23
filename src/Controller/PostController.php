5d8be93e12eebcc957d1e510ce838d2d66824309
David Gonzalo Mercado Juanes <dmercado@abc.gob.bo>
2025-05-22 23:09:43 -0400

    [CLASE 15] Guardando datos

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -3,17 +3,29 @@
 namespace App\Controller;
 
 use App\Form\PostForm;
+use Doctrine\ORM\EntityManagerInterface;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class PostController extends AbstractController
 {
     #[Route('/post/crear', name: 'post_create')]
-    public function create(): Response
+    public function create(Request $request, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(PostForm::class);
+        
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            $entityManager->persist($form->getData());
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Publicación creada con éxito');
+            return $this->redirectToRoute('post_create');
+        }
+
         return $this->render('post/create.html.twig', [
             'form' => $form->createView(),
         ]);
--- src/Form/PostForm.php
+++ src/Form/PostForm.php
@@ -4,11 +4,13 @@ namespace App\Form;
 
 use App\Entity\Category;
 use App\Entity\Post;
+
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
 // use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
